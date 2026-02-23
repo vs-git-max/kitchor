7ca0b97a9b0c887a5ff3b6312824b6b3a58e4c1f
sam chris <vsgitsam25@gmail.com>
2026-02-23 14:45:39 +0300

    added the onClose as a dependency

--- app/components/nav/MobileMenu.tsx
+++ app/components/nav/MobileMenu.tsx
@@ -36,7 +36,7 @@ export default function MobileMenu({ onClose }: MobileMenuProps) {
         "mousedown",
         handleCloseMobileMenuIncaseClickOutside,
       );
-  }, []);
+  }, [onClose]);
 
   return (
     <div
